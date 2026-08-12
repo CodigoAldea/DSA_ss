2363af1a7e6358b666b47abcdb3c37af1226ec4e
CodigoAldea <dmdndrd@gmail.com>
2026-08-12 11:09:25 +0530

    new DSA update

---
+++ main.py
@@ -0,0 +1,25 @@
+var = 10 
+# array 
+var_array = [1,2,3,4,5]
+var_array[3] = 100
+1000 + ( 3 * 4) = 1012
+
+# Insertion 
+var_array = [1,2,3,4,5]
+# moving the elements on the right side as per the size of the data. 
+
+var_array = [1,2,3, ,4,5]
+# insert the required element at the required index.
+
+var_array = [1,2,3,100,4,5]
+# output 
+var_array = [1,2,3,100,4,5]
+
+# deletion 
+var_array = [1,2,3,4,5]
+
+# deleting the element 
+var_array = [1,2,3, ,5]
+
+# shift the remaining elements to the left.
+var_array = [1,2,3,5]
